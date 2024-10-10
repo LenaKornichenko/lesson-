8233c58e90f3601d96760e22492d88dcb82ef1f6
LenaKornichenko <lenakornicenko@gmail.com>
2024-10-10 22:13:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# практическое задание "переменная"
+
+how_many_homework = 12
+print(how_many_homework)
+hours = 1.5
+print(hours)
+name_curse = 'python'
+print(name_curse)
+time_at_1_homework = hours/how_many_homework
+print(time_at_1_homework)
+print('курс:', name_curse, 'всего задач:',how_many_homework, 'затрачено часов:',
+      hours, 'cреднее время выполнения:' , time_at_1_homework, 'часа')
